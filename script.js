c6497e21e570d3bfc6c562c17c0c16b08663d4a2
Biraj Shrestha <86864374+Biraj1234@users.noreply.github.com>
2023-03-20 14:13:39 +0545

    [feat] when clicked on a particular location marker is shown and lat/long is displayed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Map Initialization
 var map = L.map("map").setView([27.685198313732105, 85.32034253942389], 21);
-
+var currentMarker = null;
 //OSM layer
 var osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -15,11 +15,18 @@ var googleStreets = L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={
 googleStreets.addTo(map);
 
 // Marker
-var marker = L.marker([27.685198313732105, 85.32034253942389]);
-var popup = marker.bindPopup("This is Ekbana").openPopup();
+
+// var popup = marker.bindPopup("This is Ekbana").openPopup();
 
 map.on("click", function (e) {
-  console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
-});
+  $("#lat").val(e.latlng.lat);
+  $("#long").val(e.latlng.lng);
+  var marker = L.marker([e.latlng.lat, e.latlng.lng]);
 
-marker.addTo(map);
+  if (currentMarker) {
+    map.removeLayer(currentMarker);
+  }
+
+  currentMarker = L.marker(e.latlng);
+  currentMarker.addTo(map);
+});
